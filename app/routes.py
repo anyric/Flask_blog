04d10ae4cb605f76f5a12a067485ed6fc881d05a
anyric <anyamaronyango@gmail.com>
2018-05-15 22:36:31 +0300

    Add view templates 	app/route.py	Add mock posts object and refactored to use templates 	app/templates/base.html	Add base template with common features 	app/templates/index.html	Add derived template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-  return "Hello World!"
+  user = {'username': 'Anyama'}
+  posts = [{
+    'author': {'username':'Anyama'},
+    'body': 'Beautiful Day in Moyo'
+    },
+    {
+    'author': {'username': 'Robert'},
+    'body': 'What a wonderfuly city, Arua!'
+    },
+    {
+    'author': {'username': 'Wilfred'},
+    'body': 'Great hopitality of West nilers'
+    }]
+  return render_template('index.html', user=user, title="Home", posts=posts)
